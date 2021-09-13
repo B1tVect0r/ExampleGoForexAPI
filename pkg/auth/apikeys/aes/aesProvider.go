c2131a1682b5cb4a6e2fb4b6c091fbd494f53f8d
Justyn Huculak <justyn.huculak@gmail.com>
2021-09-13 13:10:13 -0600

    Fully implements AES key provider

--- pkg/auth/apikeys/aes/aesProvider.go
+++ pkg/auth/apikeys/aes/aesProvider.go
@@ -1,18 +1,79 @@
 package aes
 
 import (
+	"context"
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"encoding/base64"
+	"encoding/json"
+	"fmt"
+	"io"
+
 	"github.com/b1tvect0r/exchangerates/pkg/auth/apikeys"
+	"github.com/b1tvect0r/exchangerates/pkg/db"
+	"golang.org/x/crypto/bcrypt"
 )
 
 type aesAPIKeyProvider struct {
 	key []byte
 }
 
-func (akp *aesAPIKeyProvider) Create(projectID string) (string, error) {
-	return "", nil
+func (akp *aesAPIKeyProvider) Create(ctx context.Context, q *db.Queries, kp apikeys.APIKeyParameters) (string, error) {
+	c, err := aes.NewCipher(akp.key)
+	if err != nil {
+		return "", fmt.Errorf("failed to create AES cipher: %w", err)
+	}
+
+	gcm, err := cipher.NewGCM(c)
+	if err != nil {
+		return "", fmt.Errorf("failed to create GCM operation mode: %w", err)
+	}
+
+	nonce := make([]byte, gcm.NonceSize())
+	if _, err := io.ReadFull(rand.Reader, nonce); err != nil {
+		return "", fmt.Errorf("failed to create nonce: %w", err)
+	}
+
+	hashedSecret, err := bcrypt.GenerateFromPassword(kp.Secret, bcrypt.DefaultCost)
+	if err != nil {
+		return "", fmt.Errorf("failed to hash key secret: %w", err)
+	}
+
+	kpBytes, err := json.Marshal(&kp)
+	if err != nil {
+		return "", fmt.Errorf("failed to marshal key params to JSON: %w", err)
+	}
+
+	_, err = q.SetProjectSecret(ctx, db.SetProjectSecretParams{ProjectID: kp.ProjectID, HashedSecret: hashedSecret})
+	if err != nil {
+		return "", fmt.Errorf("failed to save hashed secret to database: %w", err)
+	}
+
+	// Seal & base-64 encode the key parameters. This makes it opaque to the outside world.
+	return base64.URLEncoding.Strict().EncodeToString(gcm.Seal(nonce, nonce, kpBytes, nil)), nil
 }
 
-func (akp *aesAPIKeyProvider) Verify(apiKey string) error {
+func (akp *aesAPIKeyProvider) Verify(ctx context.Context, q *db.Queries, apiKey string) error {
+	kpBytes, err := base64.URLEncoding.Strict().DecodeString(apiKey)
+	if err != nil {
+		return fmt.Errorf("failed to decode api key: %w", err)
+	}
+
+	var kp *apikeys.APIKeyParameters
+	if err = json.Unmarshal(kpBytes, kp); err != nil {
+		return fmt.Errorf("failed to unmarshal JSON: %w", err)
+	}
+
+	hashedSecret, err := q.GetProjectSecret(ctx, kp.ProjectID)
+	if err != nil {
+		return fmt.Errorf("failed to retrieve project secret: %w", err)
+	}
+
+	if err = bcrypt.CompareHashAndPassword(hashedSecret, kp.Secret); err != nil {
+		return fmt.Errorf("API key secret mismatch: %w", err)
+	}
+
 	return nil
 }
 
--- pkg/auth/apikeys/provider.go
+++ pkg/auth/apikeys/provider.go
@@ -1,6 +1,30 @@
 package apikeys
 
+import (
+	"context"
+	"math/rand"
+
+	"github.com/b1tvect0r/exchangerates/pkg/db"
+)
+
+type APIKeyParameters struct {
+	ProjectID string `json:"ProjectID"`
+	Secret    []byte `json:"Secret"`
+}
+
 type APIKeyProvider interface {
-	Create(projectID string) (string, error)
-	Verify(apiKey string) error
+	Create(ctx context.Context, q *db.Queries, kp APIKeyParameters) (string, error)
+	Verify(ctx context.Context, q *db.Queries, apiKey string) error
+}
+
+var secretRunes = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
+
+const secretLength = 32
+
+func MakeSecret() []byte {
+	r := make([]rune, secretLength)
+	for i := range r {
+		r[i] = secretRunes[rand.Intn(len(secretRunes))]
+	}
+	return []byte(string(r))
 }
